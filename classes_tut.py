0ae2e719a21191aaa2d77865ea1d7b03e64d99af
venu <venureddy9353287822@gmail.com>
2025-12-06 21:54:57 +0530

    class_variables_part2

--- classes_tut.py
+++ classes_tut.py
@@ -1,4 +1,5 @@
 class student:
+	percentage_raise_in_fee=1.05
 	def __init__(self,name,age,branch,fee):
 		self.name=name
 		self.age=age
@@ -6,8 +7,12 @@ class student:
 		self.fee=fee
 	def email(self):
 		return self.name+"@gmail.com"
+	def raise_fee(self):
+		return self.fee*self.percentage_raise_in_fee
 std1=student("jack",22,"cse",2000)
 std2=student("rgv",19,"ece",3000)
 print(std1.name)
 print(std1.email())
-print(student.email(std2))print("class variables part 2") 
+print(student.email(std2))
+print(std1.raise_fee())
+print(std2.raise_fee())
